7ff24a6c0986815be6941699c50a524d81bb7e63
Leopold Lemmermann <leopold@lemmermann.xyz>
2024-11-19 10:02:04 +0000

    work in progress.

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "data")
+train_path = os.path.join(DATA_PATH, "NER-de-train.tsv")
+
+df = pd.read_csv(
+    train_path, sep="\t", names=["index", "token", "label"])
+
+df.dropna(inplace=True)
+df.reset_index(drop=True, inplace=True)
+print(df.head())
