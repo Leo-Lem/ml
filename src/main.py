fd611a7a2ecb973e88afb11bb36535ef334614ab
Leopold Lemmermann <leopold@lemmermann.xyz>
2024-11-20 15:15:46 +0000

    Fixed loading.

--- src/main.py
+++ src/main.py
@@ -5,8 +5,12 @@ DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "data")
 train_path = os.path.join(DATA_PATH, "NER-de-train.tsv")
 
 df = pd.read_csv(
-    train_path, sep="\t", names=["index", "token", "label"])
+    train_path,
+    sep="\t",
+    names=["index", "token", "label", "_eval"],
+    quoting=3
+)
 
 df.dropna(inplace=True)
 df.reset_index(drop=True, inplace=True)
-print(df.head())
+print(df.head(5))
