63aeaa0eacbb9caa2f43e2f51564634da5c8d1ba
Leopold Lemmermann <leopold@lemmermann.xyz>
2024-11-20 15:20:23 +0000

    Added the spacy built-in labels.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
+import spacy
 import pandas as pd
 import os
 
 DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "data")
+SPACY_LABELS = ["LOC", "MISC", "ORG", "PER"]
+
 train_path = os.path.join(DATA_PATH, "NER-de-train.tsv")
 
 df = pd.read_csv(
     train_path,
     sep="\t",
     names=["index", "token", "label", "_eval"],
+    comment="#",
     quoting=3
 )
 
-df.dropna(inplace=True)
-df.reset_index(drop=True, inplace=True)
 print(df.head(5))
+
+nlp = spacy.load("de_core_news_sm")
