811a96a933b4d19747e7fb00227ca4cfa9e58851
Leopold Lemmermann <leopold@lemmermann.xyz>
2024-11-21 08:46:38 +0000

    Added label replacement and loading/saving the model.

--- README.md
+++ README.md
@@ -59,3 +59,37 @@ You have to use the provided scripts for the evaluation of your results. <span s
 ```sh
 perl conlleval_ner.pl < your_classified_file
 ```
+
+For executing the evaluation script, you need to have perl 5 installed. Copy the evaluation script and the file to be evaluated (here: `eval-sample-d.tsv`) in the same directory.
+
+**Note**: For the file to be evaluated, you can keep the outer column all as "O".
+
+```sh
+perl nereval.perl < eval-sample-d.tsv
+> STRICT: Found: 10 outer and 5 inner phrases; Gold: 12 (outer) and 1 (inner).
+> LOOSE: Found: 10 outer and 5 inner phrases; Gold: 12 (outer) and 1 (inner).
+> 
+> 1. Strict, Combined Evaluation (official):
+> Accuracy:  92.47%;
+> Precision:  40.00%;
+> Recall:  46.15%;
+> FB1:  42.86
+> 
+> 2. Loose, Combined Evaluation:
+> Accuracy:  93.84%;
+> Precision:  53.33%;
+> Recall:  61.54%;
+> FB1:  57.14
+> 
+> 3.1 Per-Level Evaluation (outer chunks):
+> Accuracy:  91.78%;
+> Precision:  60.00%;
+> Recall:  50.00%;
+> FB1:  54.55
+> 
+> 3.2 Per-Level Global Evaluation (inner chunks):
+> Accuracy:  93.15%;
+> Precision:   0.00%;
+> Recall:   0.00%;
+> FB1:   0.00
+```
--- src/main.py
+++ src/main.py
@@ -1,20 +1,57 @@
+import random
+from spacy.util import minibatch
+from spacy.training import Example
 import spacy
 import pandas as pd
 import os
 
-DATA_PATH = os.path.join(os.path.dirname(__file__), "..", "data")
-SPACY_LABELS = ["LOC", "MISC", "ORG", "PER"]
+base_path = os.path.join(os.path.dirname(__file__), "..")
+data_path = os.path.join(base_path, "data")
+model_path = os.path.join(base_path, "model")
+paths = {
+    "train": os.path.join(data_path, "NER-de-train.tsv"),
+    "dev": os.path.join(data_path, "NER-de-dev.tsv"),
+    "sample": os.path.join(data_path, "NER-de-sample.tsv"),
+}
 
-train_path = os.path.join(DATA_PATH, "NER-de-train.tsv")
+LABELS = {
+    "PER": "PER",
+    "LOC": "LOC",
+    "ORG": "ORG",
+    "MISC": "OTH",
+}
 
+
+print("Loading the model…")
+nlp = spacy.load("de_core_news_sm")
+nlp.from_disk(model_path)
+ner = nlp.get_pipe("ner")
+
+
+print("Preparing the data…")
 df = pd.read_csv(
-    train_path,
+    paths["sample"],
     sep="\t",
     names=["index", "token", "label", "_eval"],
     comment="#",
     quoting=3
 )
 
-print(df.head(5))
+# TODO: ?:part|deriv)?
+for label, replacement in LABELS.items():
+    df["label"] = df["label"].replace(replacement, label)
 
-nlp = spacy.load("de_core_news_sm")
+for label in df["label"].unique():
+    ner.add_label(label)
+
+
+print("Training the model…")
+# TODO: train using train and dev dataset
+nlp.to_disk(model_path)
+
+
+print("Testing the model…")
+doc = nlp("Die Deutsche Flugsicherung (DFS) beschloss ein Flugverbot für alle internationalen Flughäfen mit Ausnahme der beiden Berliner Flughäfen bis 2.00 Uhr nachts.")
+for ent in doc.ents:
+    print(ent.text, ent.label_)
+# TODO: test using test dataset
